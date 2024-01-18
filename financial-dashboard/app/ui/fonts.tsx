c858f1db40231024b33bbfb6c782e216524f89d3
Ilham Pradipta Listiyanto <55991471+AMTATO@users.noreply.github.com>
2024-01-18 23:11:11 +0700

    Financial dashboard - Chapter 5

--- financial-dashboard/app/ui/fonts.tsx
+++ financial-dashboard/app/ui/fonts.tsx
@@ -1,3 +1,9 @@
-import { Inter } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
