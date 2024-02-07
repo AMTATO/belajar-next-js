09377cd4197961cd572430794fe03093b735b441
Ilham Pradipta Listiyanto <55991471+AMTATO@users.noreply.github.com>
2024-02-07 14:16:54 +0700

    Anime List - Sesi 1

---
+++ anime-list/app/search/page.tsx
@@ -0,0 +1,12 @@
+import AnimeList from "../components/AnimeList";
+
+const page = () => {
+  return (
+    <>
+      <div>Search Page</div>
+      <AnimeList></AnimeList>
+    </>
+  );
+};
+
+export default page;
